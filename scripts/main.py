6d3859a0759fdf83323afd9da5d62627d947e6e1
mmendes1 <miekal2021@gmail.com>
2025-03-30 19:25:32 -0400

    Using main for the answers testing for now

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,20 @@
 from create_tables import create_tables
+from copy_data import copy_data, get_data_file_names
+from answers import overdrawn_checking, overpaid_loans
 
 def main():
-  pass
+    overdrawn_checking()
+    overpaid_loans()
+
+    # This is all to make the db tables and load data
+    # create_tables()
+    #files = get_data_file_names()
+    #if(files is None or len(files) == 0):
+    #    print('No non-schema csv files found in data directory.')
+    #    pass # What about this? Is this right?
+    #else:  
+    #    copy_data(files)
      
 
 if __name__ == '__main__':
-   main()
+    main()
