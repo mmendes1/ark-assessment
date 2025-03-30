96dc59c9288be4e6acd304b4f56e6214dbf5c753
mmendes1 <miekal2021@gmail.com>
2025-03-30 19:22:16 -0400

    Fixing spacing + removing unneeded code

--- scripts/config.py
+++ scripts/config.py
@@ -6,14 +6,12 @@ def load_config(filename='database.ini', section='postgresql'):
 
   config={}
   if parser.has_section(section):
-    params = parser.items(section)
-    for param in params:
-      config[param[0]] = param[1]
+      params = parser.items(section)
+      for param in params:
+          config[param[0]] = param[1]
   else:
-    raise Exception('Section {0} not found in the {1} file'.format(section, filename));
+      raise Exception('Section {0} not found in the {1} file'.format(section, filename));
   return config
 
 if __name__ == '__main__':
-  config = load_config()
-  print('Config running...')
-  print(config)
+    config = load_config()
