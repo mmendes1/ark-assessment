3da1f2e13046b2d5969f4e93166a7f94cf7e4e75
mmendes1 <miekal2021@gmail.com>
2025-03-30 19:22:59 -0400

    Fixing spacing + adding comment

--- scripts/connect.py
+++ scripts/connect.py
@@ -1,16 +1,17 @@
 import psycopg2 as pg
 from config import load_config
-  
+
+# Not sure if I should be using the load_config() call as a default arg
 def connect(config=load_config()):
   try:
-    # Attempt connection to PostreSQL server
-    print(config)
-    with pg.connect(**config) as conn:
-      print('Connected to the PostgreSQL server')
-      return conn
+      # Attempt connection to PostreSQL server
+      print(config)
+      with pg.connect(**config) as conn:
+          print('Connected to the PostgreSQL server')
+          return conn
   except (pg.DatabaseError, Exception) as error:
-    print(error)
+      print(error)
 
 if __name__ == '__main__':
-  config = load_config()
-  connect(config)
+    config = load_config()
+    connect(config)
